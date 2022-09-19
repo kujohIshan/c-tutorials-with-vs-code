65e02187c56cb03d1710d208da7e6a9b6339701d
Noobmaster@69 <chakrabortyishan49@gmail.com>
2022-09-19 12:41:36 +0530

    positive_integer_in_an_array

---
+++ strings.c/Easy_print.c
@@ -0,0 +1,6 @@
+#include <stdio.h> 
+
+int main(){
+char st[] = "yare yare daze";
+printf("%s", st);
+return 0; }
---
+++ strings.c/basic_input.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+
+int main(){
+char s[69];
+printf(" enter your name\n");
+gets( s);
+printf(" your name is %s", s);
+return 0; }
