7bc481fbd77acc0c0fc1d2d88bd305d87648f1ac
Yareyare16daze <chakrabortyishan49@gmail.com>
2023-05-27 17:34:36 +0530

    init

---
+++ array.c/leftshift.c
@@ -0,0 +1,8 @@
+#include <stdio.h> 
+int main(){
+int num =10;
+int c= num<<2;
+int d =num>>4;
+printf("after left shift the value is %d \n",c);
+printf("after right shift the value is %d",d);
+return 0; }
