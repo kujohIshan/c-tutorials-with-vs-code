48450a6ea00179ca22bbceb8b19af8970cf27072
Yareyare16daze <chakrabortyishan49@gmail.com>
2025-02-03 20:48:54 +0530

    init

---
+++ java_oops/Casting.java
@@ -0,0 +1,10 @@
+public class Casting{
+    public static void main(String[] args){
+        long a= 10;
+        // cast long to int
+        int b =(int) a;
+
+        int x =Integer.parseInt("7");
+        System.out.println(x);
+    }
+}
